d6b4ac0f2ed8df026ada777abfcb229f99b3ef64
Jun-Yoon1 <jun.yn.work@gmail.com>
2025-12-18 03:25:25 +0000

    feat: binet

---
+++ fibo.py
@@ -0,0 +1,11 @@
+import math
+
+def binet(n):
+    sqrt5 = math.sqrt(5)
+    phi = (1 + sqrt5) / 2
+    psi = (1 - sqrt5) / 2
+    return round((phi ** n - psi ** n) / sqrt5)
+
+
+if __name__ == '__main__':
+    print(binet(6))
